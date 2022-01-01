f82fbbce75b3d9654ee20eac686b082143bb3337
mslmyrtd <mslmyrtd@gmail.com>
2022-01-01 15:46:50 +0300

    end

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,32 @@
 import React from "react";
-
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./LoginSytle";
+import mealSvg from "../../assets/meal.svg";
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    window.location.href = "/";
+  };
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={mealSvg} />
+        <Header>{"<Mslm/>Recipe"}</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput type="text" placeholder="username" required />
+          <StyledInput type="password" placeholder="password" required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
