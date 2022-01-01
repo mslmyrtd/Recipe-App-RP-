4061467a51f311fc3a2eb35fb9c331d66caf7b00
mslmyrtd <mslmyrtd@gmail.com>
2022-01-01 13:26:16 +0300

    RecipeCardComp added and show

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import React, { useState } from "react";
 import Header from "../../components/header/Header";
+import { HomeImg, ImgDiv, MainContainer } from "./HomeStyle";
+import RecipeCardComp from "./RecipeCardComp";
+import homeSvg from "../../assets/home.svg";
 const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
 const APP_ID = "4e9f05eb";
 const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
@@ -10,23 +13,39 @@ const Home = () => {
   const [recipes, setRecipes] = useState();
   const [meal, setMeal] = useState(mealTypes[0].toLowerCase());
 
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealTypes={meal}`;
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
   const getData = async () => {
     if (query !== "") {
       const result = await axios(url);
-      console.log(result);
-    }
+      console.log(result.data.hits);
+      if (result.data.more) console.log("no food a this name");
+      setRecipes(result.data.hits);
+      setQuery("");
+    } else console.log("Please fill the form");
   };
 
   return (
-    <Header
-      setQuery={setQuery}
-      query={query}
-      getData={getData}
-      mealTypes={mealTypes}
-      setMeal={setMeal}
-      meal={meal}
-    />
+    <>
+      <Header
+        setQuery={setQuery}
+        query={query}
+        getData={getData}
+        mealTypes={mealTypes}
+        setMeal={setMeal}
+        meal={meal}
+      />
+      {recipes ? (
+        <MainContainer>
+          {recipes.map((recipe, index) => (
+            <RecipeCardComp key={index} recipe={recipe.recipe} />
+          ))}
+        </MainContainer>
+      ) : (
+        <ImgDiv>
+          <HomeImg src={homeSvg} />
+        </ImgDiv>
+      )}
+    </>
   );
 };
 
